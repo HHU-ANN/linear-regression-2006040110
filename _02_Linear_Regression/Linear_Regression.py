805aff16ee37436b5f49b9ba719d537e2d16c993
2006040110 <131932612+2006040110@users.noreply.github.com>
2023-05-06 13:25:04 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -17,10 +17,11 @@ def ridge(data):
 
 
 def lasso(data):
-
     x, y = read_data()
-    w = lasso_regression(x,y)
-    return data@w[:6] + w[-1]
+    w = np.array([1, 1, 1, 1, 1, 1])
+
+    w = np.dot(np.linalg.inv(np.dot(x.T, x)), np.dot(x.T, y)-0.5*w)
+    return w @ data
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
